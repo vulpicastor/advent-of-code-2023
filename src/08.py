bd72b3b483734ac00541390f208c9d8c71dd1733
Lizhou Sha <lizhou@lsha.net>
2023-12-07 23:41:26 -0600

    Day 8 refactor & cleanup

--- src/08.py
+++ src/08.py
@@ -1,65 +1,28 @@
 #!/usr/bin/env python3
 
-# pylint: disable=unused-import
-import collections
 import functools
-import io
 import itertools
-import multiprocessing
-import operator as op
-import re
-import timeit
 
 import numpy as np
 import aocd
-
-import tulun
 
 YEAR = 2023
 DAY = 8
 
 
-def parse_input(data_block):
-    paths = {}
-    for l in data_block.split('\n'):
-        if not l:
-            continue
-        paths[l[:3]] = (l[7:10], l[12:15])
-    return paths
-
-
-def traverse(directions, paths, dest='ZZZ'):
-    now = 'AAA'
+def traverse(directions, paths, start='AAA', stop_func=lambda s: s=='ZZZ'):
+    now = start
     for i, d in enumerate(itertools.cycle(directions)):
-        # print(d)
         now = paths[now][int(d == 'R')]
-        # print(now)
-        if now == dest:
+        if stop_func(now):
             return i + 1
 
 
 def parallel_traverse(directions, paths):
-    now = set(d for d in paths.keys() if d[-1] == 'A')
-    print(now)
-    for i, d in enumerate(itertools.cycle(directions)):
-        choice = int(d == 'R')
-        now = set(paths[n][choice] for n in now)
-        if all(n[-1] == 'Z' for n in now):
-            print(now)
-            return i + 1
-
-
-def find_cycle(directions, paths, start):
-    now = start
-    for i, d in enumerate(itertools.cycle(directions)):
-        now = paths[now][int(d == 'R')]
-        if now[-1] == 'Z':
-            return i + 1
-
-
-# def find_cycles(directions, paths):
-    # starts = set(d for d in paths.keys() if d[-1] == 'A')
-
+    my_find_cycle = functools.partial(
+        traverse, directions, paths, stop_func=lambda s: s[-1]=='Z')
+    starts = set(d for d in paths.keys() if d[-1] == 'A')
+    return np.lcm.reduce(list(map(my_find_cycle, starts)))
 
 
 def main():
@@ -75,7 +38,7 @@ ZZZ = (ZZZ, ZZZ)
 """
     data = """LR
 
-11A = (11B, XXX)
+AAA = (11B, XXX)
 11B = (XXX, 11Z)
 11Z = (11B, XXX)
 22A = (22B, XXX)
@@ -86,18 +49,13 @@ XXX = (XXX, XXX)
 """
     data = aocd.get_data(day=DAY, year=YEAR)
     dirs, data_block = data.split('\n\n')
-    paths = parse_input(data_block)
-    print(paths)
+    paths = {l[:3]: (l[7:10], l[12:15]) for l in data_block.split('\n') if l}
 
-    # answer = traverse(dirs, paths)
-    # print(answer)
-    # aocd.submit(answer, part='a', day=DAY, year=YEAR)
+    answer = traverse(dirs, paths)
+    print(answer)
+    aocd.submit(answer, part='a', day=DAY, year=YEAR)
 
-    my_find_cycle = functools.partial(find_cycle, dirs, paths)
-    starts = set(d for d in paths.keys() if d[-1] == 'A')
-    cycles = list(map(my_find_cycle, starts))
-    print(cycles)
-    answer = np.lcm.reduce(cycles)
+    answer = parallel_traverse(dirs, paths)
     print(answer)
     aocd.submit(answer, part='b', day=DAY, year=YEAR)
 
